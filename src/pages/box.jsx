9ce2220933393708b12ce9de08a234449d224bb4
smartfox1203 <smartfox1203@gmail.com>
2022-05-27 23:45:42 +0900

    first commit

---
+++ src/pages/box.jsx
@@ -0,0 +1,30 @@
+import Instructions from '@/components/dom/Instructions'
+import dynamic from 'next/dynamic'
+
+const Box = dynamic(() => import('@/components/canvas/Box'), {
+  ssr: false,
+})
+
+// Step 5 - delete Instructions components
+const Page = () => {
+  return (
+    <>
+      <Instructions />
+    </>
+  )
+}
+Page.r3f = (
+  <>
+    <Box route='/' />
+  </>
+)
+
+export default Page
+
+export async function getStaticProps() {
+  return {
+    props: {
+      title: 'Box',
+    },
+  }
+}
